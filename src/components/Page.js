33097d842b60b636cb270d5f6782b19d5fd971f1
Jinjae Lee <jinjae.lee@icloud.com>
2020-09-26 21:15:37 +0900

    change theme, faq, coc added

--- src/components/Page.js
+++ src/components/Page.js
@@ -3,7 +3,9 @@ import React from "react";
 export function Page({ children, center }) {
   return (
     <div className={center ? "container-center-page" : "container-full-page"}>
-      <div style={{ marginTop: 40, marginBottom: 40 }}>{children}</div>
+      <div style={{ marginTop: 40, marginBottom: 40, width: "100%" }}>
+        {children}
+      </div>
     </div>
   );
 }
