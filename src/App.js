a70f2e3bc0d246611cb3ad7203203c662c02dfe3
Jinjae Lee <jinjae.lee@icloud.com>
2020-09-26 10:54:58 +0900

    Add react router

--- src/App.js
+++ src/App.js
@@ -7,108 +7,42 @@ import instaLogo from "./img/logo/Instagram.svg";
 import fbLogo from "./img/logo/Facebook.svg";
 import spectrumLogo from "./img/logo/Spectrum.svg";
 
-import { SpeakerList } from "./components/SpeakerCard";
+import { SpeakerPage, SpeakerPageTemplate } from "./components/Speaker";
+import { Page } from "./components/Page";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
+import testData from "./data";
 
 export default function App() {
-  const test = [
-    {
-      date: "11/23",
-      speaker: "김지홍",
-      company: "디자인 스펙트럼",
-      title: "디자인 위크 2020",
-      time: "18:00-20:00",
-      link: "url"
-    },
-    {
-      date: "11/23",
-      speaker: "김지홍",
-      company: "디자인 스펙트럼",
-      title: "디자인 위크 2020",
-      time: "18:00-20:00",
-      link: "url"
-    },
-    {
-      date: "11/23",
-      speaker: "김지홍",
-      company: "디자인 스펙트럼",
-      title: "디자인 위크 2020",
-      time: "18:00-20:00",
-      link: "url"
-    },
-    {
-      date: "11/23",
-      speaker: "김지홍",
-      company: "디자인 스펙트럼",
-      title: "디자인 위크 2020",
-      time: "18:00-20:00",
-      link: "url"
-    },
-    {
-      date: "11/23",
-      speaker: "김지홍",
-      company: "디자인 스펙트럼",
-      title: "디자인 위크 2020",
-      time: "18:00-20:00",
-      link: "url"
-    },
-    {
-      date: "11/23",
-      speaker: "김지홍",
-      company: "디자인 스펙트럼",
-      title: "디자인 위크 2020",
-      time: "18:00-20:00",
-      link: "url"
-    },
-    {
-      date: "11/23",
-      speaker: "김지홍",
-      company: "디자인 스펙트럼",
-      title: "디자인 위크 2020",
-      time: "18:00-20:00",
-      link: "url"
-    },
-    {
-      date: "11/23",
-      speaker: "김지홍",
-      company: "디자인 스펙트럼",
-      title: "디자인 위크 2020",
-      time: "18:00-20:00",
-      link: "url"
-    }
-  ];
   return (
-    <div className="App">
-      <Header />
-      <div className="content">
-        <Page center={true}>
-          <img className="main-video" src={MainVideo} alt={"video goes here"} />
-        </Page>
-        <Page center={true}>
-          <div className="title">
-            <div className="heading-1">
-              Spectrum Con <br />
-              Design Week 2020
-            </div>
-          </div>
-          <div className="heading-2">Nov 23, 2020 - Dec 4, 2020</div>
-          <div className="body-1">
-            디자이너들이 자신의 가치를 확인하고 더욱 발전하기 위해서는 여러
-            각도에서의 자극과 탐구가 필요합니다. <br />
-            그것이 온라인이든 오프라인이든 상관없이 디자이너들이 이러한 필요를
-            느낄 때 자유롭게 찾아와서 이야기할 수 있는 <br />
-            기회와 공간이 필요하다고 생각했습니다.
-          </div>
-        </Page>
-        <Page center={false}>
-          <div className="title">
-            <div className="heading-1">Program</div>
-          </div>
-          <div className="body-1">2주간 진행되는 다양한 세션을 들어보세요.</div>
-          <SpeakerList data={test} />
-        </Page>
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Switch>
+          <Route exact path="/">
+            <Home data={testData} />
+          </Route>
+          {testData.map((x, i) => {
+            return (
+              <Route path={`/${x.link}`} key={i}>
+                <SpeakerPageTemplate
+                  localData={x}
+                  globalData={testData}
+                  index={i}
+                />
+              </Route>
+            );
+          })}
+        </Switch>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
@@ -117,7 +51,9 @@ export function Header() {
     <div className="header">
       <div className="container-header">
         <div>
-          <img src={SCDWlogo} alt="Spectrum Con Design Week 2020" />
+          <NavLink to="/">
+            <img src={SCDWlogo} alt="Spectrum Con Design Week 2020" />
+          </NavLink>
         </div>
         <div className="heading-2">Nov 23, 2020 - Dec 4, 2020</div>
       </div>
@@ -137,10 +73,29 @@ export function Footer() {
   );
 }
 
-export function Page({ children, center }) {
+export function Home({ data }) {
   return (
-    <div className={center ? "container-center-page" : "container-full-page"}>
-      <div>{children}</div>
+    <div className="content">
+      <Page center={true}>
+        <img className="main-video" src={MainVideo} alt={"video goes here"} />
+      </Page>
+      <Page center={true}>
+        <div className="title">
+          <div className="heading-1">
+            Spectrum Con <br />
+            Design Week 2020
+          </div>
+        </div>
+        <div className="heading-2">Nov 23, 2020 - Dec 4, 2020</div>
+        <div className="body-1">
+          디자이너들이 자신의 가치를 확인하고 더욱 발전하기 위해서는 여러
+          각도에서의 자극과 탐구가 필요합니다. <br />
+          그것이 온라인이든 오프라인이든 상관없이 디자이너들이 이러한 필요를
+          느낄 때 자유롭게 찾아와서 이야기할 수 있는 <br />
+          기회와 공간이 필요하다고 생각했습니다.
+        </div>
+      </Page>
+      <SpeakerPage data={data} />
     </div>
   );
 }
---
+++ src/components/Speaker.js
@@ -0,0 +1,174 @@
+import React from "react";
+import { motion } from "framer-motion";
+import Arrow from "../img/Arrow.svg";
+import { Page } from "./Page";
+import BG1 from "../img/bg1.svg";
+import BG2 from "../img/bg2.svg";
+import BG3 from "../img/bg3.svg";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+
+export function SpeakerPage({ data }) {
+  return (
+    <Page center={false}>
+      <div className="title">
+        <div className="heading-1">Program</div>
+      </div>
+      <div className="body-1">2주간 진행되는 다양한 세션을 들어보세요.</div>
+      <SpeakerList data={data} />
+
+      <img
+        style={{
+          position: "absolute",
+          top: 0,
+          left: -480,
+          pointerEvents: "none",
+          zIndex: -10
+        }}
+        src={BG1}
+        alt={"bg3"}
+      />
+      <img
+        style={{
+          position: "absolute",
+          top: 820,
+          left: -800,
+          pointerEvents: "none",
+          zIndex: -10
+        }}
+        src={BG2}
+        alt={"bg3"}
+      />
+      <img
+        style={{
+          position: "absolute",
+          top: 440,
+          left: 460,
+          pointerEvents: "none",
+          zIndex: -10
+        }}
+        src={BG3}
+        alt={"bg3"}
+      />
+    </Page>
+  );
+}
+
+export function SpeakerList({ data }) {
+  return (
+    <div
+      style={{
+        marginTop: 80,
+        display: "grid",
+        gridRowGap: 72,
+        gridTemplateColumns: "max-content max-content",
+        gridColumnGap: 120,
+        marginBottom: 80
+      }}
+    >
+      {data.map((x, i) => {
+        return (
+          <Link to={x.link} key={i}>
+            <SpeakerCard data={x} />
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
+
+export function SpeakerCard({ data }) {
+  return (
+    <motion.div
+      whileHover={{ scale: 1.1 }}
+      style={{
+        position: "relative",
+        height: 278
+      }}
+    >
+      <div className="speaker-card-bg"></div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "auto 185px",
+          width: 500,
+          position: "relative"
+        }}
+      >
+        <div style={{ marginTop: 40, marginLeft: 30 }}>
+          <div className="speaker-card-small-text-3">{data.date}</div>
+          <div
+            style={{
+              display: "grid",
+              gridGap: 4,
+              marginTop: 30
+            }}
+          >
+            <div className="speaker-card-small-text-2">
+              {data.speaker}, {data.company}
+            </div>
+            <div className="speaker-card-small-text-1">{data.title}</div>
+            <div className="speaker-card-small-text-4">{data.time}</div>
+          </div>
+        </div>
+        <div style={{ width: 185, height: 185, backgroundColor: "#999" }}></div>
+        <motion.div
+          style={{
+            position: "absolute",
+            width: 72,
+            height: 72,
+            // backgroundColor: "blue",
+            right: 0,
+            top: 164
+          }}
+          whileHover={{ x: 20 }}
+        >
+          <img src={Arrow} alt={"arrow"} />
+        </motion.div>
+      </div>
+    </motion.div>
+  );
+}
+
+export function SpeakerPageTemplate({ localData, globalData, index }) {
+  return (
+    <div className="content">
+      <Page>
+        <NavLink to="/">
+          <div className="heading-1">{localData.speaker}</div>
+        </NavLink>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "max-content max-content",
+            gridGap: 120
+          }}
+        >
+          {index !== 0 && (
+            <div>
+              <div className="heading-2">이전 프로그램</div>
+              <NavLink to={`/${globalData[index - 1].link}`}>
+                <SpeakerCard data={globalData[index - 1]} />
+              </NavLink>
+            </div>
+          )}
+          {index !== globalData.length - 1 && (
+            <div>
+              <div className="heading-2">다음 프로그램</div>
+
+              <NavLink to={`/${globalData[index + 1].link}`}>
+                <SpeakerCard data={globalData[index + 1]} />
+              </NavLink>
+            </div>
+          )}
+        </div>
+      </Page>
+    </div>
+  );
+}
