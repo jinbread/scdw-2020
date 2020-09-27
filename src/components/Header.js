2d408fd7e32f0d7c80244fc2e365f5a79f596a2a
Jinjae Lee <jinjae.lee@icloud.com>
2020-09-27 23:58:26 +0900

    no ide

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion, useViewportScroll } from "framer-motion";
+import { motion } from "framer-motion";
 import { BrowserRouter as Router, NavLink } from "react-router-dom";
 
 export function Header({ children }) {
